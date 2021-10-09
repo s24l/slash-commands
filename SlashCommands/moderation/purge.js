a60ade7b6f9ad9d508de7f04577134a215542d60
unknown <saloomfifa167@gmail.com>
2021-10-10 02:21:10 +0300

    cooldowns added finally

--- SlashCommands/moderation/purge.js
+++ SlashCommands/moderation/purge.js
@@ -1,38 +1,37 @@
-const { CommandInteraction } = require("discord.js");
+const { CommandInteraction, Message } = require("discord.js");
 module.exports = {
-  name: "purge",
-  description: "Purge an amount of messages in a channel",
-  options: [
-    {
-      name: "amount",
-      description: "Amount to purge",
-      type: "NUMBER",
-      required: true,
-    },
-    {
-      name: "channel",
-      description: "Channel to purge",
-      type: "CHANNEL",
-      required: false,
-    },
-  ],
-   /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
+    name: "purge",
+    description: "Purge an amount of messages in a channel",
+    options: [
+        {
+            name: "amount",
+            description: "Amount to purge",
+            type: "NUMBER",
+            required: true,
+        },
+    ],
+    /**
+      *
+      * @param {Client} client
+      * @param {CommandInteraction} interaction
+      * @param {String[]} args
+      */
     run: async (client, interaction, args) => {
-    let amount = args[0].value;
-    let channel = args[1]?.channel || interaction.channel;
-    let allMessages = await channel.messages.fetch({
-      limit: amount == 100 ? amount : amount + 1,
-    });
-    let msgs = await channel.bulkDelete(allMessages);
-    await interaction.reply(`Deleted ${msgs.size}/${amount} messages`);
-    await channel.send(`This channel has been purged(${msgs.size}/${amount})`);
-    setTimeout(async () => {
-      await interaction.deleteReply();
-    }, 5000);
-  },
+
+        const query = args[0];
+
+        if (!query) interaction.followUp(`type the amount u dumbfuck`)
+
+        if (isNaN(query)) return interaction.followUp('amount must be a number');
+
+        let channel = interaction.channel;
+
+        let msgs = await channel.bulkDelete(query);
+
+        await interaction.followUp(`Deleted ${msgs.size}/${query} messages`);
+
+        setTimeout(async () => {
+            await interaction.deleteReply();
+        }, 5000);
+    },
 };
